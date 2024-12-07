54ed0b4674e41a7e65af8efe7ed1ec0d99203967
vivekchauhan_01 <122085629+Vivekchauhan1299@users.noreply.github.com>
2024-12-07 15:48:41 +0000

    variables

---
+++ Chai/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 112244;
+let accountEmail = "vivek@123"
+var password = "12345"
+accountCity = "jaipur"
+
+let accountState
+// accountId = 111            not allowed
+
+accountEmail = "codespace@123"
+password = "11111"
+accountCity = "bangluru"
+
+
+console.table([accountId,accountEmail,password,accountCity,accountState])
